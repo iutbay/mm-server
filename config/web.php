4226ac747452e168d44904712e0cfe22163e908c
Kevin LEVRON <kevin.levron@gmail.com>
2017-04-27 00:14:49 +0200

    init

--- config/web.php
+++ config/web.php
@@ -25,12 +25,31 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                'api/<action>' => 'mm/api/<action>',
             ],
+        ],
+        'fs' => [
+            'class' => 'creocoder\flysystem\LocalFilesystem',
+            'path' => '@webroot/upload',
         ],
     ],
     'modules' => [
         'mm' => [
             'class' => 'iutbay\yii2\mm\Module',
+//            'fsComponent' => 'fs',
+//            'apiOptions' => [
+//                'cors' => [
+//                    'Origin' => ['*'],
+//                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+//                    'Access-Control-Request-Headers' => ['*'],
+//                    'Access-Control-Allow-Credentials' => null,
+//                    'Access-Control-Max-Age' => 86400,
+//                    'Access-Control-Expose-Headers' => [],
+//                ],
+//            ]
+//            'thumbsPath' => '@webroot/thumbs',
+//            'thumbsUrl' => '@web/thumbs',
+//            'thumbsSize' => [150, 150],
         ],
     ],
 ];
